119e17d54e7e9b82a1111ec8e3933466735d57df
ssakarski <ssakarski96@gmail.com>
2022-01-10 23:19:11 +0200

    Rewrote a bit of bad code.

--- OMDB kursova/MainPage.xaml.cs
+++ OMDB kursova/MainPage.xaml.cs
@@ -46,75 +46,71 @@ namespace OMDB_kursova
 
             // Taking the movie title from the text box
             string title = movieSearch.Text;
-            
+
             //Getting the movie info from the API
-            var json = await client.GetStringAsync(new Uri("http://www.omdbapi.com/?t="+title+ "&apikey=6484425e"));
-
-            // Deserializing the json information
-            var mov = JsonConvert.DeserializeObject<RootObject>(json);
-
-            // If the title is not found the response will be false and the title will be set to Titanic
-            if(Convert.ToBoolean(mov.Response)!= true || (title == "" || title == "not available"))
+            try
             {
-                title = "Titanic";
-                json = await client.GetStringAsync(new Uri("http://www.omdbapi.com/?t=" + title + "&apikey=6484425e"));
-                mov = JsonConvert.DeserializeObject<RootObject>(json);
+                var json = await client.GetStringAsync(new Uri("http://www.omdbapi.com/?t=" + title + "&apikey=6484425e"));
+                // Deserializing the json information
+                var mov = JsonConvert.DeserializeObject<RootObject>(json);
+
+
+                // If the title is not found the response will be false and the title will be set to Titanic
+
+
+                //if (Convert.ToBoolean(mov.Response) == false || (title == "" || title == "not available"))
+                //{
+                //    title = "Titanic";
+                //    json = await client.GetStringAsync(new Uri("http://www.omdbapi.com/?t=" + title + "&apikey=6484425e"));
+                //    mov = JsonConvert.DeserializeObject<RootObject>(json);
+                //}
+
+                // Parsing the json to html to get the image path string
+                var Poster = new HtmlParser().Parse(mov.Poster);
+                string posterPath = Poster.Body.TextContent;
+
+                // Using BitmapImage get the image from the provided Uri
+                BitmapImage imageBitmap = new BitmapImage(new Uri(posterPath));
+                //Assigning the value of the BitmapImage to the Xaml image box
+                movPoster.Source = imageBitmap;
+
+                //Assigning the information from Omdb to the text fields in xaml.
+
+                movTitle.Text += mov.Title;
+
+                movYear.Text += mov.Year;
+
+                movRated.Text += mov.Rated;
+
+                movReleased.Text += mov.Released;
+
+                movRuntime.Text += mov.Runtime;
+
+                movGenre.Text += mov.Genre;
+
+                movDirector.Text += mov.Director;
+
+                movWriter.Text += mov.Writer;
+
+                movActor.Text += mov.Actors;
+
+                movPlot.Text += mov.Plot;
+
+                movLang.Text += mov.Language;
+
+                movCountry.Text += mov.Country;
+
+                movAwards.Text += mov.Awards;
+
+                movImdbRating.Text += mov.imdbRating;
+
+                movSeasons.Text += mov.totalSeasons;
+            }
+            catch
+            {
+                movieSearch.Text = "The title was not found or you have no connection.";
             }
 
-            // Parsing the json to html to get the image path string
-            var Poster = new HtmlParser().Parse(mov.Poster);
-            string posterPath = Poster.Body.TextContent;
-            // Using BitmapImage get the image from the provided Uri
-           
-            BitmapImage imageBitmap = new BitmapImage(new Uri(posterPath));
-            //Assigning the value of the BitmapImage to the Xaml image box
-            movPoster.Source = imageBitmap;
-
-            // Html parsing and assigning the other information(Title/Year/Genre etc.) about the movie
-            var Title = new HtmlParser().Parse(mov.Title);
-            movTitle.Text += Title.Body.TextContent;
-
-            var Year = new HtmlParser().Parse(mov.Year);
-            movYear.Text += Year.Body.TextContent;
-
-            var Rated = new HtmlParser().Parse(mov.Rated);
-            movRated.Text += Rated.Body.TextContent;
-
-            var Released = new HtmlParser().Parse(mov.Released);
-            movReleased.Text += Released.Body.TextContent;
-
-            var Runtime = new HtmlParser().Parse(mov.Runtime);
-            movRuntime.Text += Runtime.Body.TextContent;
-
-            var Genre = new HtmlParser().Parse(mov.Genre);
-            movGenre.Text += Genre.Body.TextContent;
-
-            var Director = new HtmlParser().Parse(mov.Director);
-            movDirector.Text += Director.Body.TextContent;
-
-            var Writer = new HtmlParser().Parse(mov.Writer);
-            movWriter.Text += Writer.Body.TextContent;
-
-            var Actor = new HtmlParser().Parse(mov.Actors);
-            movActor.Text += Actor.Body.TextContent;
-
-            var Plot = new HtmlParser().Parse(mov.Plot);
-            movPlot.Text += Plot.Body.TextContent;
-
-            var Language = new HtmlParser().Parse(mov.Language);
-            movLang.Text += Language.Body.TextContent;
-
-            var Country = new HtmlParser().Parse(mov.Country);
-            movCountry.Text += Country.Body.TextContent;
-
-            var Awards = new HtmlParser().Parse(mov.Awards);
-            movAwards.Text += Awards.Body.TextContent;
-
-            var IMDBRating = new HtmlParser().Parse(mov.imdbRating);
-            movImdbRating.Text += IMDBRating.Body.TextContent;
-
-            var Seasons = new HtmlParser().Parse(mov.totalSeasons);
-            movSeasons.Text += Seasons.Body.TextContent;
         }
 
         private void Clear_Click(object sender, RoutedEventArgs e)
